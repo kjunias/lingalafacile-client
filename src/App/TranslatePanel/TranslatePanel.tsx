4ee10a8e625bdb7a262db514b2028ddd77bd44d6
Junias Ngoyi <kjunias@gmail.com>
2018-12-18 02:15:02 -0500

    Added inputs

--- src/App/TranslatePanel/TranslatePanel.tsx
+++ src/App/TranslatePanel/TranslatePanel.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import './TranslatePanel.css';
 import TranslateControls from './TranslateControls/TranslateControls';
+import SearchArea from './SearchArea/SearchArea';
 
 class TranslatePanel extends React.Component {
     render() {
       return (
         <div className="translate-panel">
           <TranslateControls/>
+          <SearchArea/>
         </div>
       );
     }
