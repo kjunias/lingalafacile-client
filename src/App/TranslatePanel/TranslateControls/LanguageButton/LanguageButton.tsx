893654d9afc6cf54c84995550681a7758d3077cf
Junias Ngoyi <kjunias@gmail.com>
2018-12-18 20:02:03 -0500

    Lang button css & clickhandler

--- src/App/TranslatePanel/TranslateControls/LanguageButton/LanguageButton.tsx
+++ src/App/TranslatePanel/TranslateControls/LanguageButton/LanguageButton.tsx
@@ -6,14 +6,30 @@ export interface Props {
   className?: string;
 }
 
-class LanguageButton extends React.Component <Props, object> {
+export interface LangState {
+  isActive: boolean;
+}
+
+class LanguageButton extends React.Component <Props, LangState> {
+
   constructor (props: Props) {
     super(props);
+    this.state = { isActive: false };
+  }
+
+  onLangClick = () => {
+    this.setState({
+      isActive: true
+    });
   }
 
   render() {
     return (
-      <button className={'lang-button ' + (this.props.className ? this.props.className : '')}>
+      <button 
+        className={'lang-button' + (this.props.className ? ' ' + this.props.className : '') 
+          + (this.state.isActive ? ' active' : '')}
+        onClick={this.onLangClick}
+      >
         {this.props.name}
       </button>
     );
