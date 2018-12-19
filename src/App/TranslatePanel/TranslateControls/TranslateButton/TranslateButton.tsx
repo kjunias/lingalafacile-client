96e9a80bcba99e29ee8043ff28a105e9d5ad5dcf
Junias Ngoyi <kjunias@gmail.com>
2018-12-19 13:00:49 -0500

    Refactored translate button

---
+++ src/App/TranslatePanel/TranslateControls/TranslateButton/TranslateButton.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './TranslateButton.css';
+
+class TranslateButton extends React.Component <object> {
+
+  constructor (props: object) {
+    super(props);
+    this.state = { isActive: false };
+  }
+
+  render() {
+    return (
+      <button className="lang-button translate">
+        Translate
+      </button>
+    );
+  }
+}  
+export default TranslateButton;
