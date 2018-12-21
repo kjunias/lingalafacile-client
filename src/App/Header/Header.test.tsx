98e437fc6b1d0adf65627fdb691da74af6fdbba0
Junias Ngoyi <kjunias@gmail.com>
2018-12-20 22:40:17 -0500

    Tested header

--- src/App/Header/Header.test.tsx
+++ src/App/Header/Header.test.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as enzyme from 'enzyme';
 import Header from './Header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
+});
+
+it('contains correct child elements', () => {
+  const header = enzyme.mount(<Header />);
+
+  expect(header.find(".header .hamburgermenu").length).toEqual(1);
+  expect(header.find(".header .topmenu").length).toEqual(1);
 });
