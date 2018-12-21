2a7be0775d636e4c8776f8a506a9d7061583b9a0
Junias Ngoyi <kjunias@gmail.com>
2018-12-20 19:44:05 -0500

    Fixed tslint errors

--- src/App/Header/HamburgerMenu/HamburgerMenu.tsx
+++ src/App/Header/HamburgerMenu/HamburgerMenu.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './HamburgerMenu.css';
-var imgSrc = require('./images/ham_menu.png');
+import imgSrc from './images/ham_menu.png';
 
 class HamburgerMenu extends React.Component {
-    render() {
-      return (
-        <div className="hamburgermenu">
-            <img src={imgSrc} />
-        </div>
-      );
-    }
-  }  
+  public  render() {
+    return (
+      <div className="hamburgermenu">
+          <img src={imgSrc} />
+      </div>
+    );
+  }
+}
 export default HamburgerMenu;
