be938f29ae1ac4a9876cb5069233596b225ffd0d
Junias Ngoyi <kjunias@gmail.com>
2018-12-21 18:46:34 -0500

    Setup redux libs in TranslateCtrls

--- src/App/TranslatePanel/TranslateControls/TranslateControls.tsx
+++ src/App/TranslatePanel/TranslateControls/TranslateControls.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './TranslateControls.css';
 import LanguageButton from './LanguageButton/LanguageButton';
 import SwapButton from './SwapButton/SwapButton';
@@ -13,6 +15,12 @@ export interface ITranslateCtrlState {
 export interface ILanguage {
   name: string;
 }
+
+const translateCtrlReducer: Redux.Reducer = (state: ITranslateCtrlState, action: Redux.Action) => {
+  return state;
+};
+
+const translateCtrlStore: Redux.Store = Redux.createStore(translateCtrlReducer);
 
 class TranslateControls extends React.Component<object, ITranslateCtrlState> {
 
@@ -38,22 +46,24 @@ class TranslateControls extends React.Component<object, ITranslateCtrlState> {
 
   public render() {
     return (
-      <div className="translate-controls">
-        <div className="left">
-          {Languages.map((lang: ILanguage, i: string) =>
-            <LanguageButton key={i} name={lang.name} active={this.state.from.active} setActive={this.setActiveFrom} />
-          )}
+      <ReactRedux.Provider store={translateCtrlStore}>
+        <div className="translate-controls">
+          <div className="left">
+            {Languages.map((lang: ILanguage, i: string) =>
+              <LanguageButton key={i} name={lang.name} active={this.state.from.active} setActive={this.setActiveFrom} />
+            )}
+          </div>
+          <div className="center">
+            <SwapButton />
+            <TranslateButton />
+          </div>
+          <div className="right">
+            {Languages.map((lang: ILanguage, i: string) =>
+              <LanguageButton key={i} name={lang.name} active={this.state.to.active} setActive={this.setActiveTo} />
+            )}
+          </div>
         </div>
-        <div className="center">
-          <SwapButton />
-          <TranslateButton />
-        </div>
-        <div className="right">
-          {Languages.map((lang: ILanguage, i: string) =>
-            <LanguageButton key={i} name={lang.name} active={this.state.to.active} setActive={this.setActiveTo} />
-          )}
-        </div>
-      </div>
+      </ReactRedux.Provider>
     );
   }
 }
